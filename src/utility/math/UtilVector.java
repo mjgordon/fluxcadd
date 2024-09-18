7ded59c3e58ee41875686741e2986302620f6ea3
mjgordon <mjgordon@users.noreply.github.com>
2024-09-18 21:00:55 +0300

    D : Added some docstrings to UtilVector

--- src/utility/math/UtilVector.java
+++ src/utility/math/UtilVector.java
@@ -3,17 +3,28 @@ package utility.math;
 import org.joml.Matrix4d;
 import org.joml.Vector3d;
 
+/**
+ * Utilities on top of Vector3d
+ *
+ */
 public class UtilVector {
+	
+	/**
+	 * Get the shortest transformation matrix between two Vectors
+	 * @param a
+	 * @param b
+	 * @return
+	 */
 	public static Matrix4d getTransformVecVec(Vector3d a, Vector3d b) {
 		Vector3d v = new Vector3d(a).cross(b);
-		//double s = v.length();
+
 		double c = a.dot(b);
 		
 		Matrix4d vx = new Matrix4d();
 		
 		vx.m00(0);
 		vx.m01(v.z);
-		vx.m02(- v.y);
+		vx.m02(-v.y);
 		
 		vx.m10(-v.z);
 		vx.m11(0);
