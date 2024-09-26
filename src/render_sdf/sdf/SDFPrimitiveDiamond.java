b2ca050ec9c8d2ca440a96870c546dc484a6d8c9
mjgordon <mjgordon@users.noreply.github.com>
2024-09-26 15:45:52 +0300

    C : Corrected distance function for SDFPrimitiveDiamond

--- src/render_sdf/sdf/SDFPrimitiveDiamond.java
+++ src/render_sdf/sdf/SDFPrimitiveDiamond.java
@@ -1,8 +1,5 @@
 package render_sdf.sdf;
 
-import static java.lang.Math.abs;
-
-
 import org.joml.Matrix4d;
 import org.joml.Vector3d;
 import org.joml.Vector4d;
@@ -18,14 +15,17 @@ import utility.Color;
 public class SDFPrimitiveDiamond extends SDF {
 	private Matrix4dAnimated frame;
 
-	private double size;
+	private double halfSize;
+	
+	private double axisSize;
 
 
 	public SDFPrimitiveDiamond(Vector3d position, double size, Material material) {
 		Matrix4d base = new Matrix4d().setColumn(3, new Vector4d(position,1));
 		frame = new Matrix4dAnimated(base, "Diamond");
 		
-		this.size = size;
+		this.halfSize = size / 2;
+		this.axisSize = Math.sqrt(Math.pow(size, 2) / 2);
 		this.material = material;
 		
 		displayName = "PrimDiamond";
@@ -34,9 +34,54 @@ public class SDFPrimitiveDiamond extends SDF {
 
 	@Override
 	public double getDistance(Vector3d v, double time) {
-		Vector3d vLocal = v.mulPosition(frame.getInvert(time),new Vector3d());
-
-		return abs(vLocal.x) + abs(vLocal.y) + abs(vLocal.z) - size;
+		Vector3d vl = v.mulPosition(frame.getInvert(time),new Vector3d());
+		vl.absolute();
+		
+		double sum = vl.x + vl.y + vl.z;
+		double offset = (sum - axisSize) / 3;
+		
+		double faceX = vl.x - offset;
+		double faceY = vl.y - offset;
+		double faceZ = vl.z - offset;
+		
+		boolean bx = faceX >= 0;
+		boolean by = faceY >= 0;
+		boolean bz = faceZ >= 0;
+		
+		
+		if (!bx && !by) {
+			return vl.distance(0, 0, axisSize);
+		}
+		else if (!by && !bz) {
+			return vl.distance(axisSize, 0, 0);
+		}
+		else if (!bz && !bx) {
+			return vl.distance(0, axisSize, 0);
+		}
+		else if (!bx) {
+			double yzSum = vl.y + vl.z - axisSize;
+			double yzOffset = yzSum / 2;
+			return vl.distance(0, vl.y - yzOffset, vl.z - yzOffset);
+		}
+		else if (!by) {
+			double xzSum = vl.x + vl.z - axisSize;
+			double xzOffset = xzSum / 2;
+			return vl.distance(vl.x - xzOffset, 0, vl.z - xzOffset);
+		}
+		else if (!bz) {
+			double xySum = vl.x + vl.y - axisSize;
+			double xyOffset = xySum / 2;
+			return vl.distance(vl.x - xyOffset,  vl.y - xyOffset, 0);
+		}
+		// Point is within the projected face
+		else if (bx && by && bz) {
+			return vl.distance(faceX, faceY, faceZ);
+		}
+		else {
+			System.out.println("this shouldn't happen");
+		}
+		
+		return Double.NaN;
 	}
 	
 	
@@ -44,7 +89,7 @@ public class SDFPrimitiveDiamond extends SDF {
 	public void extractSceneGeometry(GeometryDatabase gd, boolean solid, boolean materialPreview, double time) {
 		Group g = new Group();
 		
-		float hp = (float) (size / 2);
+		float hp = (float) (axisSize / 2);
 		
 		Color c = getPrimitiveColor(solid, materialPreview);
 		
@@ -56,6 +101,7 @@ public class SDFPrimitiveDiamond extends SDF {
 		
 		gd.add(g);
 	}
+	
 	
 	@Override
 	public Animated[] getAnimated() {
--- test_scripts/testSDFPrimitiveDiamond.scm
+++ test_scripts/testSDFPrimitiveDiamond.scm
@@ -3,7 +3,7 @@
 (let* ((material-ground (MaterialDiffuse. (Color. "3D5A80") 0))
        (material-diamond (MaterialDiffuse. (Color. "98C1D9") 0)))
 
-  (add-camera-position-keyframe 0 20.0 5.0 7.0)
+  (add-camera-position-keyframe 0 20.0 5.0 20.0)
   (add-camera-target-keyframe 0 0.0 0.0 10.0)
 
   (set-scene-sdf (SDFPrimitiveGroundPlane. 0 material-ground))
